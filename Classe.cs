7bf7271cfb1d814329831a31db43f0a5a1a56b72
ddouglss <ddouglss1999@gmail.com>
2024-10-24 21:59:09 -0300

    Lista

---
+++ Classe.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Course {
+    internal class Classe {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+#pragma warning disable CS8618 // O campo não anulável precisa conter um valor não nulo ao sair do construtor. Considere declará-lo como anulável.
+        public Classe(int id, string name, double salary) {           
+            Id = id;
+            Name = name;
+            Salary = salary;        
+        }
+#pragma warning restore CS8618 // O campo não anulável precisa conter um valor não nulo ao sair do construtor. Considere declará-lo como anulável.
+
+        public void IncreseSalary(double percentage) {
+            Salary += Salary * percentage / 100.0;
+        }
+
+        public override string ToString() {
+
+            return Id
+                + ","
+                + Name
+                + ","
+                + Salary.ToString("F2", CultureInfo.InvariantCulture);
+
+        
+        }
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("How many employees will be registered?");
+            int how = int.Parse(Console.ReadLine());
+            
+            List<Classe> list = new List<Classe>();
+
+            for (int i = 1; i < how; i++) {
+                Console.WriteLine("Employee #" + i + ":");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary=double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new Classe(id, name, salary));
+                Console.WriteLine();
+
+            }
+            
+            Console.Write("Enter the employee id that will have salary increase : ");
+            int searchId = int.Parse(Console.ReadLine());
+
+            Classe emp = list.Find(x => x.Id == searchId);
+            if (emp != null) {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.IncreseSalary(percentage);
+            }
+            else {
+                Console.WriteLine("This id does not exist!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees:");
+            foreach (Classe obj in list) {
+                Console.WriteLine(obj);
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
